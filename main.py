76c26c32f7a175a3921e809ca400a0496fdacbb2
rodri <rodrigo.roman.mm@gmail.com>
2023-02-01 20:20:35 +0100

    Initial steps

---
+++ main.py
@@ -0,0 +1,15 @@
+from gtts import gTTS
+from gtts import langs
+from test_gtts import *
+
+if __name__ == "__main__":
+    def end_function():
+        print("That's all folks!")
+
+
+# tts = gTTS('Hello again, my dear and beloved friend')
+# tts.save('hello.mp3')
+
+say_hello()
+
+end_function()
