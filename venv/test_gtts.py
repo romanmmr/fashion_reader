6f7a15e17da7fce92549f538751e517d2c2384e4
rodri <rodrigo.roman.mm@gmail.com>
2023-02-05 19:59:23 +0100

    feat: first staps web scrapping

--- venv/test_gtts.py
+++ venv/test_gtts.py
@@ -6,3 +6,9 @@ def say_hello():
     # tts = gTTS(lang="en", "Hello my potatoe!")
     tts = gTTS("Hola mi patata", lang="es", tld="es")
     tts.save('hello.mp3')
+
+
+# import pyttsx3
+# engine = pyttsx3.init()
+# engine.say("I will speak this text")
+# engine.runAndWait()
